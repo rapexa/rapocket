e029c79307dcaaa496dc1dd955e85ec9fc8ce9fa
rapexa <rapexamrs@gmail.com>
2022-02-10 15:30:03 +0330

    commit settings.py.sample

--- rapocket/web/models.py
+++ rapocket/web/models.py
@@ -8,13 +8,15 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User,on_delete=models.CASCADE)
-    def __unicode__(self):
-        return self.text
+    
+    def __str__(self):
+        return f"({self.text})  {self.date}  ({self.amount})"
     
 class Income(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User,on_delete=models.CASCADE)
-    def __unicode__(self):
-        return self.text
+    
+    def __str__(self):
+        return f"({self.text})  {self.date}  ({self.amount})"
