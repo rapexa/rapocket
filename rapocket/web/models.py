6f33618de395a212e6a4cdd4908fe36ea5425372
rapexa <rapexamrs@gmail.com>
2022-02-10 15:11:28 +0330

    add Income to project and admin webpage

--- rapocket/web/models.py
+++ rapocket/web/models.py
@@ -1,8 +1,15 @@
+from cgitb import text
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
 class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    
+class Income(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
