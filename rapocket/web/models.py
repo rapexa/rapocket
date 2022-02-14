537db2f94a3fd6e009711ee25229404b2e246715
rapexa <rapexamrs@gmail.com>
2022-02-14 16:56:52 +0330

    adding unicode for tokens & codeing post requests for submit expense to send expense with post requests

--- rapocket/web/models.py
+++ rapocket/web/models.py
@@ -5,6 +5,10 @@ from django.contrib.auth.models import User
 class Token(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     token = models.CharField(max_length=48)
+    
+    def __str__(self):
+        return f"{self.user}_token"
+    
 class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
@@ -13,6 +17,7 @@ class Expense(models.Model):
     
     def __str__(self):
         return f"({self.text})  {self.date}  ({self.amount})"
+    
     
 class Income(models.Model):
     text = models.CharField(max_length=255)
--- rapocket/web/views.py
+++ rapocket/web/views.py
@@ -1,7 +1,24 @@
+from json import JSONEncoder
+from lib2to3.pgen2 import token
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from web.models import User,Token,Expense,Income
+import datetime
 
 # Create your views here.
+@csrf_exempt
 def submit_expense(request):
     """user submit an expense"""
     
-    print("we are here !")
+    this_token = request.POST['token']
+    this_user = User.objects.filter( token__token = this_token).get()
+    now = datetime.datetime.now()
+    Expense.objects.create(user = this_user, amount=request.POST['amount'],text = request.POST['text'],date=now)
+    print("im in submit expense!")
+    print(request.POST)
+    
+    return JsonResponse ({
+        'status':'ok',
+        
+    }, encoder=JSONEncoder)
