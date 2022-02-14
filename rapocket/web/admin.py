21d1133e8fe80f55ee8de22b6e7c9310c0858c3a
rapexa <rapexamrs@gmail.com>
2022-02-14 16:13:35 +0330

    adding model of tokens

--- rapocket/web/admin.py
+++ rapocket/web/admin.py
@@ -1,6 +1,8 @@
+from lib2to3.pgen2 import token
 from django.contrib import admin
-from .models import Expense,Income
+from .models import Expense,Income,Token
 
 # Register your models here.
 admin.site.register(Expense)
 admin.site.register(Income)
+admin.site.register(Token)
