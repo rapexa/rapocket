1f322e4cae881757eb87ae25916500bd10ac9cff
rapexa <rapexamrs@gmail.com>
2022-02-16 14:04:17 +0330

    income submit : url,func,...

--- rapocket/web/views.py
+++ rapocket/web/views.py
@@ -8,14 +8,38 @@ import datetime
 
 # Create your views here.
 @csrf_exempt
+def submit_income(request):
+    """user submit an income"""
+    
+    #TODO: validate data, user might be fake, token might be fake,amount...
+    this_token = request.POST['token']
+    this_user = User.objects.filter( token__token = this_token).get()
+    
+    if 'date' not in request.POST:
+        date = datetime.datetime.now()
+    
+    Income.objects.create(user = this_user, amount=request.POST['amount'],text = request.POST['text'],date=date)
+    print("[+] submit income!")
+    print(request.POST)
+    
+    return JsonResponse ({
+        'status':'ok',
+        
+    }, encoder=JSONEncoder)
+
+@csrf_exempt
 def submit_expense(request):
     """user submit an expense"""
     
+    #TODO: validate data, user might be fake, token might be fake,amount...
     this_token = request.POST['token']
     this_user = User.objects.filter( token__token = this_token).get()
-    now = datetime.datetime.now()
-    Expense.objects.create(user = this_user, amount=request.POST['amount'],text = request.POST['text'],date=now)
-    print("im in submit expense!")
+    
+    if 'date' not in request.POST:
+        date = datetime.datetime.now()
+    
+    Expense.objects.create(user = this_user, amount=request.POST['amount'],text = request.POST['text'],date=date)
+    print("[+] submit expense!")
     print(request.POST)
     
     return JsonResponse ({
